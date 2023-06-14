d88142326bb47153f2ce0ca435ecf863bbfc90a8
Evgeny Stepanischev <imbolk@gmail.com>
2023-06-14 18:10:46 +0300

    Check for SectorC

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 SC=../sectorc
-cat $SC/rt/lib.c obf_hello.c $SC/rt/_start.c | $SC/run_raw.sh - 2>&-
+
+if [ ! -d "$SC" ]; then
+	echo SectorC compiler is not found
+	exit 1
+fi
+
+cat "$SC/rt/lib.c" obf_hello.c "$SC/rt/_start.c" | "$SC/run_raw.sh" - 2>&-
